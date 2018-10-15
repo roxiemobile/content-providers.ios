af17c2f0356fb1b4dc9e0dfc2a2d35d53ae48414
Alexander Bragin <alexander.bragin@gmail.com>
2018-10-15 03:06:02 +0300

    Added script for sending libraries to the Specs repo.

---
+++ pod_trunk_push.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# pod trunk push ContentProvidersFileSystem.podspec
+pod trunk push --allow-warnings ContentProvidersSQLite.podspec
+# pod trunk push ContentProvidersUserDefaults.podspec
+pod trunk push --allow-warnings ContentProviders.podspec
