e0449a038d667d6690c1e6112592a76e40f183d0
Aleksandr Serov <Serov-ae@roxiemobile.com>
2020-10-27 19:23:46 +0300

    EKMBIO-5 Fixed warnings: removed "public" inside public extension.

--- Modules/RoxieMobile.ContentProviders/Sources/SQLite/Sources/Extensions/Foundation/NSError.swift
+++ Modules/RoxieMobile.ContentProviders/Sources/SQLite/Sources/Extensions/Foundation/NSError.swift
@@ -16,21 +16,21 @@ public extension NSError
 {
 // MARK: - Construction
 
-    public convenience init(code: NSError.DatabaseError.Code, description: String? = nil) {
+    convenience init(code: NSError.DatabaseError.Code, description: String? = nil) {
         self.init(domain: DatabaseError.Domain, code: code.rawValue, userInfo: (description != nil) ? [NSLocalizedDescriptionKey: description!] : nil)
     }
 
-    public convenience init(code: NSError.FileError.Code, description: String? = nil) {
+    convenience init(code: NSError.FileError.Code, description: String? = nil) {
         self.init(domain: FileError.Domain, code: code.rawValue, userInfo: (description != nil) ? [NSLocalizedDescriptionKey: description!] : nil)
     }
 
-    public convenience init(code: NSError.SerializationError.Code, description: String? = nil) {
+    convenience init(code: NSError.SerializationError.Code, description: String? = nil) {
         self.init(domain: SerializationError.Domain, code: code.rawValue, userInfo: (description != nil) ? [NSLocalizedDescriptionKey: description!] : nil)
     }
 
 // MARK: - Properties
 
-    public class var databaseIsInvalid: NSError
+    class var databaseIsInvalid: NSError
     {
         struct Singleton {
             static let error = NSError(code: DatabaseError.Code.DatabaseIsInvalid,
@@ -39,7 +39,7 @@ public extension NSError
         return Singleton.error
     }
 
-    public class var inputIsInvalid: NSError
+    class var inputIsInvalid: NSError
     {
         struct Singleton {
             static let error = NSError(code: DatabaseError.Code.InputIsInvalid,
@@ -48,7 +48,7 @@ public extension NSError
         return Singleton.error
     }
 
-    public class var modelIsInvalid: NSError
+    class var modelIsInvalid: NSError
     {
         struct Singleton {
             static let error = NSError(code: DatabaseError.Code.ModelIsInvalid,
@@ -59,7 +59,7 @@ public extension NSError
 
 // MARK: - Constants
 
-    public struct DatabaseError
+    struct DatabaseError
     {
         // Error domain
         public static let Domain = "DatabaseErrorDomain"
@@ -72,7 +72,7 @@ public extension NSError
         }
     }
 
-    public struct FileError
+    struct FileError
     {
         // Error domain
         static let Domain = "FileErrorDomain"
@@ -83,7 +83,7 @@ public extension NSError
         }
     }
 
-    public struct SerializationError
+    struct SerializationError
     {
         // Error domain
         static let Domain = "SerializationErrorDomain"
